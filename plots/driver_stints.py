54eecf7db5c0d9409760fbc5864d7c3f109ae44b
SirWilliamIII <willcarpenterwastaken@gmail.com>
2025-02-12 03:42:48 -0600

    added maual stint detection

--- plots/driver_stints.py
+++ plots/driver_stints.py
@@ -28,20 +28,38 @@ def plot_driver_stints(year, weekend, session_type, driver="VER"):
         session = ff1.get_session(year, weekend, session_type)
         session.load()
         
-        driver_stints = session.laps.pick_driver(driver).get_stints()
+        # Get driver's laps and sort by time
+        driver_laps = session.laps.pick_driver(driver).sort_values('Time')
         
         fig, ax = plt.subplots(figsize=(10, 6))
         
-        for idx, stint in driver_stints.iterlaps():
-            ax.plot(stint['LapNumber'], 
-                   stint['LapTime'].dt.total_seconds(),
-                   marker='o',
-                   label=f'Stint {idx+1} ({stint["Compound"].iloc[0]})')
+        # Plot lap times colored by compound
+        compounds = driver_laps['Compound'].unique()
+        current_stint = 0
+        last_compound = None
+        
+        for idx, lap in driver_laps.iterrows():
+            if last_compound != lap['Compound']:
+                current_stint += 1
+                last_compound = lap['Compound']
+            
+            color = 'red' if lap['Compound'] == 'SOFT' else 'yellow' if lap['Compound'] == 'MEDIUM' else 'white'
+            ax.scatter(lap['LapNumber'], 
+                      lap['LapTime'].total_seconds(),
+                      c=color,
+                      edgecolor='black',
+                      label=f'Stint {current_stint} ({lap["Compound"]})' if idx == driver_laps.index[0] or last_compound != driver_laps.iloc[idx-1]['Compound'] else "",
+                      marker='o')
+        
+        # Remove duplicate labels
+        handles, labels = plt.gca().get_legend_handles_labels()
+        by_label = dict(zip(labels, handles))
+        ax.legend(by_label.values(), by_label.keys())
         
         ax.set_xlabel('Lap Number')
         ax.set_ylabel('Lap Time (seconds)')
         ax.set_title(f'{driver} Stint Analysis - {session.event.year} {session.event.name}')
-        ax.legend()
+        ax.grid(True, alpha=0.2)
         
         img = io.BytesIO()
         plt.savefig(img, format='png', dpi=300, bbox_inches='tight')
